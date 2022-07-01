9a1ce7f82aa32c74635de8fee3428472165901c3
pfdah <pratikdahal700@gmail.com>
2022-07-01 07:06:53 +0545

    Requesting input for indentation

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import json
 import os
 
-inputs_loc = '/home/pratik/Downloads/JSONs/Input'
-output_loc = '/home/pratik/Downloads/JSONs/Output'
+inputs_loc = 'Your Input folder location here'
+output_loc = 'Your Output location here'
 
 if __name__ == '__main__':
+    input_indt = int(input("Enter the indentation\n"))
     print("Starting")
     for js in os.listdir(inputs_loc):
         i_f = open(os.path.join(inputs_loc, js))
         data = json.load(i_f)
         with open(os.path.join(output_loc, js), 'w') as o_f:
-            json.dump([data], o_f, indent=2)
+            json.dump([data], o_f, indent=input_indt)
         i_f.close()
         o_f.close()
 
